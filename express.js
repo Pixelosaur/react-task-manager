ef7540e57125ec05812d94d884ca3d78a0896a89
christina <xristitina@gmail.com>
2020-05-07 20:52:54 +0300

    Installs express.js and set ups mock server

---
+++ express.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+/** create new express app */
+const app = express();
+
+/** prevent cross-origin error */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(cors());
+
+/** parse the body of the request */
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+
+/** mock '/' route */
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+/** express server runs at port :4000 */
+app.listen(4000, () => {
+    console.log('Express mock server listening on port 4000!');
+});
+
+module.exports = app;
