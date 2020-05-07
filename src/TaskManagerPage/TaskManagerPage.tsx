e8e6cebd9d03af88df5370fc8fe39b50c704b025
christina <xristitina@gmail.com>
2020-05-07 21:53:41 +0300

    Modifies TaskManager page layout and adds styles

--- src/TaskManagerPage/TaskManagerPage.tsx
+++ src/TaskManagerPage/TaskManagerPage.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import './TaskManagerPage.scss';
 
 export default class TaskManagerPage extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col">
-                    <NavBar />
+            <div className="container-fluid">
+                <NavBar />
+                <div className="row pt-5">
+                    <div className="col">
+                        Task List
+                    </div>
                 </div>
             </div>
         );
