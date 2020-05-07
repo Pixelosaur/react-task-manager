3aa8b2bae05fbce81dbaba13ab361ee6c5151f72
christina <xristitina@gmail.com>
2020-05-07 21:06:27 +0300

    Creates a TaskManagerPage and adds basic routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import './App.scss';
+import LoginPage from '../LoginPage/LoginPage';
+import { Redirect, Route, Switch } from 'react-router';
+import TaskManagerPage from '../TaskManagerPage/TaskManagerPage';
 
 class App extends Component {
     render() {
-        return <div>Hello React</div>;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Switch>
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/task-manager" component={TaskManagerPage} />
+                            <Redirect from="/" to="login" exact />
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
---
+++ src/TaskManagerPage/TaskManagerPage.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TaskManagerPage extends Component {
+    render() {
+        return (
+            <div className="mt-5">
+                Task Table
+            </div>
+        );
+    }
+}
