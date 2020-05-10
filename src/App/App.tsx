85a909cd9fa64a1170164a27ae308b805d92e950
christina <xristitina@gmail.com>
2020-05-10 15:13:54 +0300

    Consumes PrivateRoute on App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,13 +3,16 @@ import './App.scss';
 import LoginPage from '../LoginPage/LoginPage';
 import { Redirect, Route, Switch } from 'react-router';
 import TaskManagerPage from '../TaskManagerPage/TaskManagerPage';
+import { PrivateRoute } from '../components/PrivateRoute/PrivateRoute';
 
-class App extends Component {
+class App extends Component<any, any> {
     render() {
         return (
             <Switch>
-                <Route path="/login" component={LoginPage} />
-                <Route path="/task-manager" component={TaskManagerPage} />
+                <Route exact path="/login" component={LoginPage} />
+                <PrivateRoute exact path="/task-manager">
+                    <TaskManagerPage />
+                </PrivateRoute>
                 <Redirect from="/" to="login" exact />
             </Switch>
         );
